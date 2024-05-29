cef26053d83d28d79dfe224f1468b23a2fb79d2f
luka chikashvili <lukachikashvili2020@gmail.com>
2024-05-29 19:42:15 +0400

    add hero section

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen flex item-center justify-center pt-24'>
+       
+       <div className='flex gap-4'>
+          <h1 className='text-white ' style={{fontSize: '10rem'}}>&#402;</h1>
+        <p className='text-white ' style={{fontSize: '10rem'}}>&#8317;</p>
+        <div className='flex flex-col'>
+        <p className='text-white ' style={{fontSize: '7rem'}}> 
+        <span className='text-green-500'>Web</span> developer</p>
+         <p className='w-full h-2 bg-white '></p>
+         <div className='flex gap-12'>
+         <p className='text-white relative ' style={{fontSize: '5rem'}}>&#8730; <span className='text-2xl absolute top-8 '>Creativity</span></p>
+         <p className='text-white ml-12 mt-2  ' style={{fontSize: '3rem'}}>&#8727;</p>
+         <p className='text-white relative' style={{fontSize: '5rem'}}>&#8730; <span className='text-2xl absolute top-8 '>Passion</span></p>
+         <p className='text-white -mt-6 ml-12 ' style={{fontSize: '5rem'}}>&#8332;</p>
+         <p className='text-white -mt-2' style={{fontSize: '5rem'}}> &#8734;</p>
+       
+         </div>
+         </div>
+        <p className='text-white ' style={{fontSize: '10rem'}}>&#8318; </p>
+       </div>
+    </div>
+  )
+}
+
+export default Home
