11699883ac3a74036bc51466e03077d721bad49e
luka chikashvili <lukachikashvili2020@gmail.com>
2024-05-31 16:33:56 +0400

    add framer motion to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { motion } from 'framer-motion';
 
 const Header = () => {
   const time = new Date();
@@ -7,25 +7,41 @@ const Header = () => {
   const minutes = time.getMinutes();
   return (
     <div className='w-full flex items-center justify-between px-28 py-8 text-white  text-3xl'>
-       <div className="hidden logo md:flex gap-4 mt-4">
-         <p style={{fontSize: '70px'}} >&radic; </p>
-         <div className='flex flex-col '>
-         <span className='text-3xl absolute top-10 '>Chikashvili <p className='text-xl -right-4 text-green-500 absolute -top-4'>Luka</p></span>
+       <div
+        className="hidden logo md:flex gap-4 mt-4">
+         <motion.p initial = {{opacity: 0}}
+                      whileInView={{opacity: 1}}
+                      transition={{duration:1, delay: 1.5}}
+         style={{fontSize: '70px'}}  >&radic; </motion.p>
+         <div 
+         className='flex flex-col '>
+         <motion.span initial = {{opacity: 0}}
+                      whileInView={{opacity: 1}}
+                      transition={{duration:1, delay: 1.6}}
+         className='text-3xl absolute top-10 '>Chikashvili <p className='text-xl -right-4 text-green-500 absolute -top-4'>Luka</p></motion.span>
      
          </div>
        </div>
 
-       <nav className='flex gap-12 list-none '>
+       <motion.nav initial = {{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)'}}
+                     animate={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}
+                     transition={{ duration: 1.5, delay: 1.6 }}
+       className='flex gap-12 list-none '>
           <li><a href = "#home" data-replace = "Home"><span>Home</span></a></li>
           <li><a href = "#about" data-replace = "About"><span>About</span></a></li>
           <li><a href = "#skills" data-replace = "Skills"><span>Skills</span></a></li>
           <li><a href = "#projects" data-replace = "Projects"><span>Projects</span></a></li>
      
-       </nav>
+       </motion.nav>
 
-    <div>
-        <a href = "#" data-replace = " Tbilisi"><span> Tbilisi</span></a>
-        <span className='absolute text-xl top-6'>{hours}&#8758;{minutes}</span>
+    <div >
+        <motion.a initial = {{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)'}}
+                     whileInView={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}
+                     transition={{ duration: 1.5, delay: 1.6 }} href = "#" data-replace = " Tbilisi"><span> Tbilisi</span></motion.a>
+        <motion.span initial = {{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)'}}
+                     whileInView={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}
+                     transition={{ duration: 1.5, delay: 1.8 }}
+        className='absolute text-xl top-6'>{hours}&#8758;{minutes}</motion.span>
        
     </div>
     </div>
