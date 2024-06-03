27414ababb764b9648b4c599e99f69fd61405dd7
luka chikashvili <lukachikashvili2020@gmail.com>
2024-06-03 13:45:19 +0400

    add 3d menu animation and 3d models

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -1,20 +1,31 @@
-import React, { useContext, useEffect, useRef } from 'react'
-import {Center, Float, Html, Text3D, useGLTF, useMatcapTexture} from '@react-three/drei'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import {Center, ContactShadows, Float, Html, Text3D, useGLTF, useMatcapTexture} from '@react-three/drei'
 import { Context } from '../context/Context';
 import { useFrame } from '@react-three/fiber';
 
+
 const Three = () => {
 
-    const [texture] = useMatcapTexture('482908_894E0D_FBDB52_CA7420', 256);
+ 
     const {index} = useContext(Context);
+    const [showBook, setShowBook] = useState(false);
+    const [showTool, setShowTool] = useState(false);
+    const [showProj, setShowProj] = useState(false);
+
 
     const model = useGLTF('./house.glb');
+    const paper = useGLTF('./paper.glb');
+    const tools = useGLTF('./tools.glb');
+    const laptop = useGLTF('./model.gltf');
+
+
 
 let homeRef = useRef();
 let aboutRef = useRef();
 let skillsRef = useRef();
 let projectsRef = useRef();
 let modelRef = useRef();
+let ref = useRef();
 
 
 
@@ -27,27 +38,36 @@ let modelRef = useRef();
         projectsRef.current.position.lerp({ x: -6, y: 0, z: -5 }, lerpFactor);
         aboutRef.current.position.lerp({ x: 6, y: 0, z: -5 }, lerpFactor);
         skillsRef.current.position.lerp({ x: 20, y: 0, z: -17 }, lerpFactor);
+        setShowBook(false);
+        setShowTool(false);
+        setShowProj(false);
       } else if (index === 1) {
         aboutRef.current.position.lerp({ x: 0, y: 0, z: 0 }, lerpFactor);
         aboutRef.current.scale.lerp({ x: 3, y: 4, z: 1 }, lerpFactor);
         homeRef.current.position.lerp({ x: -6, y: 0, z: -5 }, lerpFactor);
         skillsRef.current.position.lerp({ x: 6, y: 0, z: -5 }, lerpFactor);
         projectsRef.current.position.lerp({ x: 20, y: 0, z: -17 }, lerpFactor);
+        setShowBook(true);
       } else if (index === 2) {
         skillsRef.current.position.lerp({ x: 0, y: 0, z: 0 }, lerpFactor);
         aboutRef.current.position.lerp({ x: -6, y: 0, z: -5 }, lerpFactor);
         projectsRef.current.position.lerp({ x: 6, y: 0, z: -5 }, lerpFactor);
         homeRef.current.position.lerp({ x: 20, y: 0, z: -17 }, lerpFactor);
+        setShowTool(true);
+        setShowBook(false);
       } else if (index === 3) {
         projectsRef.current.position.lerp({ x: 0, y: 0, z: 0 }, lerpFactor);
         skillsRef.current.position.lerp({ x: -6, y: 0, z: -5 }, lerpFactor);
         homeRef.current.position.lerp({ x: 6, y: 0, z: -5 }, lerpFactor);
         aboutRef.current.position.lerp({ x: 20, y: 0, z: -17 }, lerpFactor);
+        setShowProj(true);
+        setShowTool(false);
       }
   
-      modelRef.current.rotation.y += 0.001;
+      
     })
 
+    
   return (
    <>
 
@@ -67,27 +87,67 @@ shadow-camera-left={ - 10 }
 <pointLight position={[-10, -10, -10]} intensity={0.5} />
 <hemisphereLight skyColor={'#ffffff'} groundColor={'#b9b9b9'} intensity={0.9} />
 
-            <mesh scale-y = {4} scale-x = {3} rotation-x = {2.2} ref={homeRef} receiveShadow>
+            <mesh scale-y = {4} scale-x = {3} rotation-x = {2.2} ref={homeRef} receiveShadow >
               <boxGeometry />
-              <meshStandardMaterial color = "blue" />
-              
+              <meshStandardMaterial color = "#8EAC50" />
+              <Center>
+              <Html className='text-2xl text-[#FFD700] font-bold'>
+                001
+              </Html>
+              </Center>
             </mesh>
 
-            <primitive object={model.scene} scale = {0.09} position = {[-0.1, 0.2, 0.5]} rotation-x = {0.5} ref = {modelRef} castShadow />
+{showBook ?    <primitive object={paper.scene} scale = {13} position = {[0, 1, 1]} rotation-x = {0.6} ref = {modelRef} castShadow /> 
+: showTool ?   <primitive object={tools.scene} scale = {11} position = {[0, 0, 1]} rotation-x = {0.6} ref = {modelRef} castShadow /> :
+showProj ? (<>
+
+<primitive object={laptop.scene} scale = {0.7} 
+position = {[0, 0.2, 1]}  rotation-x={ 0.5 }  castShadow /> 
  
-            <mesh scale-y = {4} scale-x = {3} rotation-x = {2.2} position = {[6, 0, -5]} ref={aboutRef} castShadow>
+ 
+</>
+):
+            <primitive object={model.scene} scale = {0.09} position = {[-0.1, 0.2, 0.5]} rotation-x = {0.5} ref = {modelRef} castShadow />}
+ 
+ <ContactShadows
+    position-y={ - 1.4 }
+    opacity={ 0.4 }
+    scale={ 5 }
+    blur={ 2.4 }
+/>
+
+            <mesh  scale-y = {4} scale-x = {3} rotation-x = {2.2} position = {[6, 0, -5]} ref={aboutRef} castShadow>
+
               <boxGeometry />
-              <meshStandardMaterial color = "red" />
+              <meshStandardMaterial color = "#F72798" />
+
+              <Center>
+              <Html className='text-2xl text-[#FFD700] font-bold'>
+                002
+              </Html>
+              </Center>
             </mesh>
 
             <mesh scale-y = {4} scale-x = {3} rotation-x = {2.2} position = {[12, 0, -10]} ref={skillsRef}  castShadow>
               <boxGeometry />
-              <meshStandardMaterial color = "green" />
+              <meshStandardMaterial color = "#864AF9"/>
+
+              <Center>
+              <Html className='text-2xl text-[#FFD700] font-bold'>
+                003
+              </Html>
+              </Center>
             </mesh>
 
             <mesh scale-y = {4} scale-x = {3} rotation-x = {2.2} position = {[20, 0, -17]} ref={projectsRef}  castShadow>
               <boxGeometry />
-              <meshStandardMaterial color = "purple" />
+              <meshStandardMaterial color = "#FF5F00" />
+
+              <Center>
+              <Html className='text-2xl text-[#FFD700] font-bold'>
+                004
+              </Html>
+              </Center>
             </mesh>
  
 
